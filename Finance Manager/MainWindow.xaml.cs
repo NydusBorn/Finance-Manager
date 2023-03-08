65552881d1ec426e771c3d0b57c9d15244dac432
Nydus Born <nydusborn@outlook.com>
2023-03-08 21:32:30 +0300

    Внешний вид интерфейса

--- Finance Manager/MainWindow.xaml.cs
+++ Finance Manager/MainWindow.xaml.cs
@@ -12,17 +12,31 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Wpf.Ui.Controls;
 
 namespace Finance_Manager
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : WPF_UI.MainWindow
+    public partial class MainWindow : UiWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Tab_Users_Select(object sender, RoutedEventArgs e)
+        {
+            if (!IsInitialized) return;
+            Tc_Tabs.SelectedIndex = 0;
+            Tg_Transaction_Sel.IsChecked = false;
+        }
+        private void Tab_Transactions_Select(object sender, RoutedEventArgs e)
+        {
+            if (!IsInitialized) return;
+            Tc_Tabs.SelectedIndex = 1;
+            Tg_Users_Sel.IsChecked = false;
         }
     }
 }
